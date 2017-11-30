7e1b57de126ab1df03179b16871f804226385d00
Irving Brito <8423460+ibrito@users.noreply.github.com>
2017-11-30 12:23:23 -0300

    Crea El directorio Log para el Script iniciarServidores_20171130.sh

---
+++ Developers/iniciarLogs_20171130.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#+++++++++++++++++++++++++++++
+#title           : iniciarLogs_20171130.sh
+#description     : Crea El directorio para  log del Script iniciarServidores.sh
+#author          : IrvingBrito
+#date            : 2017-11-30
+#++++++++++++++++++++++++++++++
+#!/bin/bash
+DIRECTORIO="/var/log/MiWeb/"
+
+
+	if [ -d $DIRECTORIO ];
+		then
+			echo "Sí existe $DIRECTORIO"
+		else
+			echo "No existe $DIRECTORIO "
+			echo "Se creara el directorio $DIRECTORIO"
+			mkdir $DIRECTORIO
+			echo "se creo $DIRECTORIO"
+	fi
