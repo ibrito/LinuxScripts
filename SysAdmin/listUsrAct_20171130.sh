e0d17b924f4893222a28a7616d35b656e5f409dc
Irving Brito <8423460+ibrito@users.noreply.github.com>
2017-11-30 14:11:27 -0300

    Lista Usuarios con sesiones activas en el sistema

---
+++ SysAdmin/listUsrAct_20171130.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#+++++++++++++++++++++++++++++
+#title           : listUsrAct_20171130.sh
+#description     : Lista los Usuarios con sesiones activas en el sistema
+#author          : IrvingBrito
+#date            : 2017-11-30
+#++++++++++++++++++++++++++++++
